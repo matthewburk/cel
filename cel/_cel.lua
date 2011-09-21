1f705e8cca4b30a86d7cab40765c3761361f9e66
matthew burk <matthew.burk@gmail.com>
2011-09-20 20:58:44 -0700

    organizing

--- cel/_cel.lua
+++ cel/_cel.lua
@@ -289,7 +289,7 @@ do --ENV.describe
 
     local t = getdescription(cel)
 
-    if true or cel[_refresh] or t.id == 0 or t.refresh 
+    if cel[_refresh] or t.id == 0 or t.refresh 
       or (t.clip.l ~= gl or t.clip.r ~= gr or t.clip.t ~= gt or t.clip.b ~= gb) then
       __describe(cel, host, gx, gy, gl, gt, gr, gb, t)
 
